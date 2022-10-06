927b89af849e8defc959451bbb04ee95f62d0fcd
ageltz07 <ageltz07@gmail.com>
2022-10-06 10:44:46 -0400

    Querying Data

--- src/main/java/com/example/demo/StudentRepository.java
+++ src/main/java/com/example/demo/StudentRepository.java
@@ -1,6 +1,28 @@
 package com.example.demo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+
+import javax.transaction.Transactional;
+import java.util.List;
+import java.util.Optional;
 
 public interface StudentRepository extends JpaRepository<Student, Long> {
+    @Query("SELECT s FROM Student s WHERE s.email = ?1")
+    Optional<Student> findStudentByEmail(String email);
+
+    @Query("SELECT s FROM Student s WHERE s.firstName = ?1 AND s.age >= ?2")
+    List<Student> findStudentByFirstNameEqualsAndAgeIsGreaterThanEqual(
+            String firstName, Integer age);
+
+    @Query(
+            value = "SELECT * FROM student WHERE first_name = ?1 AND age >= ?2", nativeQuery = true)
+    List<Student> findStudentByFirstNameEqualsAndAgeIsGreaterThanEqualNative(
+            String firstName, Integer age);
+
+    @Transactional
+    @Modifying
+    @Query("DELETE FROM Student u WHERE u.id = ?1")
+    int deleteStudentById(Long id);
 }
